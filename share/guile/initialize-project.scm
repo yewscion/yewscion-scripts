c76fa4cb9b647fd736d7538984b0d16d4e84eeaa
Christopher Rodriguez <yewscion@gmail.com>
2022-11-01 20:12:56 -0400

    Ignore build dir, basic part of initialize-project.

---
+++ share/guile/initialize-project.scm
@@ -0,0 +1,28 @@
+(define-module (yewscion initialize-project)
+  :use-module (cdr255 userlib)
+  :use-module (ice-9 ftw)
+  :use-module (srfi srfi-1)
+  :use-module (srfi srfi-98)
+  :export (initialize-project))
+(define* (initialize-project #:key
+                              (project-name (dereference-env "PWD"))
+                              (template-directory
+                               (dereference-env-in-string
+                                "HOME"
+                                "$HOME/.local/share/empty-repo/"))
+                              (verbosity #f))
+  (let ((files (scandir template-directory)))
+    (when verbosity
+      (display (string-append "Project Name: \""
+                              project-name
+                              "\"\nTemplate Directory: \""
+                              template-directory
+                              "\"\nFile List: \""
+                              (string-join files ",")
+                              "\"\n")))
+    
+    ))
+
+; GOAL: Replicate `cp -LRv --no-preserve=all \
+; ~/.local/share/empty-repo/. .;chmod 775 setup-symlinks.sh bootstrap \
+; incant.sh;./setup-symlinks.sh`
