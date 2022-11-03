c41ef94dfa71447da7f70815e253b7e7a3c519d0
Christopher Rodriguez <yewscion@gmail.com>
2022-11-03 16:03:23 -0400

    Restructured to remove wrapping from calcluation logic.

--- share/guile/run-backups.scm
+++ share/guile/run-backups.scm
@@ -1,32 +1,44 @@
 (define-module (run-backups)
   :use-module (ice-9 ftw)
   :use-module (cdr255 userlib)
-  :export (run-backups))
-
+  :use-module (cdr255 combinators)
+  :export (run-backups
+           show-backups))
 (define default-backup-directories
-  (map (lambda (x)
-         (dereference-env-in-string
-          "HOME"
-          x))
-       '("$HOME/.local/bin"
-         "$HOME/Archives"
-         "$HOME/Books"
-         "$HOME/Mail"
-         "$HOME/Mirrors"
-         "$HOME/Music"
-         "$HOME/Pictures"
-         "$HOME/Research"
-         "$HOME/Videos")))
+  (dereference-env-in-string
+   "HOME"
+   (string-append "$HOME/.local/bin\n$HOME/Archives\n$HOME/Books\n$HOME/Mail"
+                  "\n$HOME/Mirrors\n$HOME/Music\n$HOME/Pictures\n"
+                  "$HOME/Research\n$HOME/Videos")))
 (define default-repository
   (dereference-env-in-string
    "HOME"
    "$HOME/Backups"))
-(define* (construct-restic-call #:key (directories
-                                       default-backup-directories)
-                                (repository
-                                 default-repository))
-  (string-append
-   "restic -r "
-   repository
-   " backup "
-   (string-join directories)))
+(define* (construct-restic-call directories repository)
+  (let ((directory-string (join-directories directories)))
+    (string-append
+     "restic -r "
+     repository
+     " backup "
+     directory-string)))
+(define* (procedure-wrapper procedure action #:rest args)
+  (apply action (list (apply procedure args))))
+(define* (show-backups #:key (directories
+                            default-backup-directories)
+                     (repository
+                      default-repository))
+  (procedure-wrapper construct-restic-call
+                     display
+                     directories
+                     repository))
+(define* (run-backups #:key (directories
+                             default-backup-directories)
+                      (repository
+                       default-repository))
+  (procedure-wrapper construct-restic-call
+                     system
+                     directories
+                     repository))
+
+(define (join-directories directories)
+  (replace-regexp-in-string "\n" " " directories))
